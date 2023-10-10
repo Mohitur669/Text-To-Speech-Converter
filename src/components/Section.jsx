d0badf5e497ce3cb80ec679c3ec6d4e4b03d1c45
Mohitur669 <mohitur669@gmail.com>
2023-10-10 18:20:46 +0530

    zoom height fix

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -7,7 +7,7 @@ const Section = ({text, setText, convertTextToSpeech}) => {
             <textarea
                 value={text}
                 style={{padding: 15}}
-                placeholder='Write your text to convert'
+                placeholder='Write your text to convert. After converting click the play button below to play.'
                 onChange={(e) => setText(e.target.value)}>
             </textarea>
             <button className='btn-convert' onClick={() => convertTextToSpeech()} >Convert to Speech</button>
