29e6062614d747be7bfa4cf1dacc50521f07e582
Mohd Mohitur Rahaman <mohitur669@gmail.com>
2024-03-08 02:04:43 +0530

    change placeholder text

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -7,7 +7,7 @@ const Section = ({text, setText, convertTextToSpeech}) => {
             <textarea className="text-area"
                 value={text}
                 style={{padding: 15}}
-                placeholder='Write your text to convert. After converting click the play button below to play.'
+                placeholder='Write your text to be converted. After converting click the play button below to play.'
                 onChange={(e) => setText(e.target.value)}>
             </textarea>
             <button className='btn-convert' onClick={() => convertTextToSpeech()} >Convert to Speech</button>
